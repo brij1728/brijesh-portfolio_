30f5a776bc5ce0c2c22b28f3f4f8c1ff18f0f7fa
Brijesh Kumar <bk10895@gmail.com>
2024-08-28 17:21:38 +0530

    style: improve page layout and typography
    - Adjust bottom padding of the main div for better space utilization
    - Refine gaps between elements for more balanced visual hierarchy in different viewports
    - Adjust font size of the main heading for improved readability
    - Align paragraph text to the start in larger viewports for better text flow

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 export default function Home() {
   return (
     <div
-      className='w-full flex flex-col md:flex-row items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48'
+      className='w-full flex flex-col md:flex-row items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48 pb-4'
       style={{ height: 'calc(100vh - 96px)' }} // Remaining height after header
     >
       {/* Image */}
@@ -17,11 +17,11 @@ export default function Home() {
         />
       </div>
       {/* Text */}
-      <div className='w-full md:w-1/2 flex flex-col gap-8 items-center justify-center p-4 h-[50vh] md:h-full text-secondary-100'>
-        <h1 className='text-3xl md:text-5xl lg:text-6xl font-bold text-start'>
+      <div className='w-full md:w-1/2 flex flex-col gap-4 md:gap-8 items-center justify-center p-4 h-[50vh] md:h-full text-secondary-100'>
+        <h1 className='text-2xl md:text-5xl lg:text-6xl font-bold text-start'>
           Crafting Digital Experience, Designing Tomorrow.
         </h1>
-        <p className='text-base md:text-lg lg:text-xl text-center'>
+        <p className='text-base md:text-lg lg:text-xl text-start md:text-center'>
           Welcome to my digital canvas, where innovation and creativity
           converge. With a keen eye for aesthetics and a mastery of code, my
           portfolio showcases a diverse collection of projects that reflect my
