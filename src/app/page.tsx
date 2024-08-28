f9556b13a733c5076109ca6bf9923ae35fb8ca9d
Brijesh Kumar <bk10895@gmail.com>
2024-08-28 17:00:06 +0530

    refactor: Update title and layout for Brijesh's portfolio
    The code changes in the layout and page components update the title of the portfolio to "Brijesh Portfolio" and adjust the layout to include a responsive header. This improves the overall design and user experience of the portfolio website.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,12 @@ import Image from 'next/image';
 
 export default function Home() {
   return (
-    <div className='min-h-screen w-full flex flex-col md:flex-row items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48  '>
+    <div
+      className='w-full flex flex-col md:flex-row items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48'
+      style={{ height: 'calc(100vh - 96px)' }} // Remaining height after header
+    >
       {/* Image */}
-      <div className='relative w-full h-[50vh] md:h-[100vh] md:w-1/2'>
+      <div className='relative w-full h-[50vh] md:h-full md:w-1/2'>
         <Image
           src='/hero.png'
           alt='Hero Image'
@@ -14,7 +17,7 @@ export default function Home() {
         />
       </div>
       {/* Text */}
-      <div className='w-full md:w-1/2 flex flex-col gap-8 items-center justify-center p-4 h-[50vh] md:h-[100vh] text-secondary-100'>
+      <div className='w-full md:w-1/2 flex flex-col gap-8 items-center justify-center p-4 h-[50vh] md:h-full text-secondary-100'>
         <h1 className='text-3xl md:text-5xl lg:text-6xl font-bold text-start'>
           Crafting Digital Experience, Designing Tomorrow.
         </h1>
