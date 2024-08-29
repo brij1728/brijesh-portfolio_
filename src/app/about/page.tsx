2ff75f4db0c5212f01f4b76d6084d288cedd8b10
Brijesh Kumar <bk10895@gmail.com>
2024-08-29 17:48:39 +0530

    refactor: Improve About page layout and add Experience section
    - Adjust layout of About page for better responsiveness and readability
    - Implement scrolling on About page for improved user experience
    - Add Experience section providing details about professional history
    - Include position title, work description, duration, and company for each experience entry

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -7,8 +7,11 @@ import { skills } from '@/data';
 import { useRef } from 'react';
 
 const AboutPage = () => {
-  const containerRef = useRef<HTMLElement | null>(null);
+  const containerRef = useRef<HTMLElement | null>(
+    null,
+  ) as React.MutableRefObject<HTMLDivElement | null>;
   const { scrollYProgress } = useScroll({ container: containerRef });
+  console.log(scrollYProgress);
   return (
     <motion.div
       className='h-full '
@@ -17,7 +20,7 @@ const AboutPage = () => {
       transition={{ duration: 1 }}
     >
       {/* CONTAINER */}
-      <div className=' h-full overflow-scroll lg:flex'>
+      <div ref={containerRef} className=' h-full overflow-scroll lg:flex '>
         {/* TEXT CONTAINER */}
         <div className='p-4 sm:p-8 md:p-12 lg:p-20 xl:p-48 flex flex-col gap-24 md:gap-32 lg:gap-48 xl:gap-64 lg:w-2/3 lg:pr-0 xl:w-1/2'>
           {/* BIOGRAPHY CONTAINER */}
