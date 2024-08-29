db38034cbd44fddee1edaaf4fb06c669eba395cd
Brijesh Kumar <bk10895@gmail.com>
2024-08-29 17:03:38 +0530

    refactor: Add Brain component to components index and About page
    - Import the Brain component in the components index file
    - Add the Brain component to the About page for displaying a brain image
    - Improve code structure and remove redundant divs in the About page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,9 +1,14 @@
 'use client';
 
-import { motion } from 'framer-motion';
+import { motion, useScroll } from 'framer-motion';
+
+import { Brain } from '@/components';
 import { skills } from '@/data';
+import { useRef } from 'react';
 
 const AboutPage = () => {
+  const containerRef = useRef<HTMLElement | null>(null);
+  const { scrollYProgress } = useScroll({ container: containerRef });
   return (
     <motion.div
       className='h-full '
@@ -186,7 +191,9 @@ const AboutPage = () => {
           </div>
         </div>
         {/* SVG CONTAINER */}
-        <div className='hidden lg:block w-1/3 sticky top-0 z-30 xl:w-1/2'></div>
+        <div className='hidden lg:block w-1/3 sticky top-0 z-30 xl:w-1/2'>
+          <Brain scrollYProgress={scrollYProgress} />
+        </div>
       </div>
     </motion.div>
   );
