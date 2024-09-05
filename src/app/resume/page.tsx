0de89b4c8e71ef19935af348310f3e7a6c20127e
Brijesh Kumar <bk10895@gmail.com>
2024-09-05 18:19:59 +0530

    feat: Add responsive scaling to ResumePage
    - Implement responsive scaling on ResumePage based on screen width
    - Use useEffect and useState hooks to update the scale state
    - Set different scale values for different screen sizes
    - Improve user experience by adjusting the scale of the PDF document accordingly

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -4,6 +4,7 @@ import 'react-pdf/dist/Page/TextLayer.css';
 import 'react-pdf/dist/Page/AnnotationLayer.css';
 
 import { Document, Page, pdfjs } from 'react-pdf';
+import { useEffect, useState } from 'react';
 
 import { Button } from '@/components/ui';
 import Link from 'next/link';
@@ -14,7 +15,41 @@ pdfjs.GlobalWorkerOptions.workerSrc = new URL(
   import.meta.url,
 ).toString();
 
-const ResumePage = () => {
+export default function ResumePage() {
+  const [scale, setScale] = useState(1);
+
+  // Function to set the scale based on screen width
+  const updateScale = (width: number) => {
+    if (width >= 1280) {
+      // xl screens (1280px and above)
+      setScale(2.0);
+    } else if (width >= 1024) {
+      // lg screens (1024px and above)
+      setScale(1.7);
+    } else if (width >= 768) {
+      // md screens (768px and above)
+      setScale(1.2);
+    } else if (width >= 640) {
+      // sm screens (640px and above)
+      setScale(1.0);
+    } else {
+      // xs screens (below 640px)
+      setScale(0.7);
+    }
+  };
+
+  useEffect(() => {
+    const handleResize = () => updateScale(window.innerWidth);
+    window.addEventListener('resize', handleResize);
+
+    // Set the initial scale
+    updateScale(window.innerWidth);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   return (
     <div className='flex flex-col items-center justify-center min-h-screen w-full bg-gradient-to-b from-blue-100 to-red-100 py-10'>
       <div className='mb-8 text-center'>
@@ -31,8 +66,11 @@ const ResumePage = () => {
         </Link>
       </div>
       <div className='flex justify-center items-center'>
-        <Document file={'/Brijesh Kumar resume.pdf'}>
-          <Page pageNumber={1} />
+        <Document
+          file={'/Brijesh Kumar resume.pdf'}
+          className='d-flex justify-content-center'
+        >
+          <Page pageNumber={1} scale={scale} />
         </Document>
       </div>
       <div className='my-8 text-center'>
@@ -50,6 +88,4 @@ const ResumePage = () => {
       </div>
     </div>
   );
-};
-
-export default ResumePage;
+}
